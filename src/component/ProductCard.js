87d0dd8868ed80ecb7af4919e4f4ad297aad5e8f
kyuri <yimkyuri@naver.com>
2024-03-28 02:14:44 +0900

    second commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://lp2.hm.com/hmgoepprod?set=source[/90/29/9029cc4507bc5a3f76281f9adf98cc6f5ad03459.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[2]&call=url[file:/product/main]" alt="" />
+      <div>제목1</div>
+      <div>제목2</div>
+      <div>가격</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async() => {
+    let url =`http://localhost:4000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect (() => {
+    getProducts()
+  },[])
   return (
     <div>
-      전체상품
+      <ProductCard />
     </div>
   )
 }
