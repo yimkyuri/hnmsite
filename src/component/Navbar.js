b41d5889bb76c8798786cecedb58b45fa4c21968
kyuri <yimkyuri@naver.com>
2024-03-28 17:29:28 +0900

    ui design

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const menuList = [
-        "메뉴1",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴5",
-        "메뉴6",
-        "메뉴7"
+        "여성",
+        "Devided",
+        "남성",
+        "신생아/유아",
+        "아동",
+        "H&M HOME",
+        "Sale",
+        "지속가능성"
     ]
   return (
     <div>
         <div>
-            <FontAwesomeIcon icon={faUser} /> <p>로그인</p>
+            <div className='login-button'>
+                <FontAwesomeIcon icon={faUser} /> <div>로그인</div>
+            </div>
         </div>
         <div>
-            로고
+            <div className='logo'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/709px-H%26M-Logo.svg.png' alt='' /></div>
         </div>
-        <div>
-            <ul>
+        <div className='menu-area'>
+            <ul className='menu-list'>
                 {menuList.map((menu) => (
                     <li>{menu}</li>
                 ))}
             </ul>
+            <div className='search-area'>
+                <FontAwesomeIcon icon={faSearch} /> <input type='text' />
+            </div>
         </div>      
     </div>
   )
